c593d1dfbe50080b3aa0fc81579da5fb389f5ffa
Gary Chow <32419809+UCONNFSAE@users.noreply.github.com>
2019-05-25 15:52:59 -0400

    Configured sensor address, emissivity, channels, sampling freq

--- Brake_Temp_Sensor_CAN/Brake_Temp_Sensor_CAN.ino
+++ Brake_Temp_Sensor_CAN/Brake_Temp_Sensor_CAN.ino
@@ -4,11 +4,14 @@
 
 #define SPI_CAN 9
 
+int temp1, temp2, temp3, temp4 = 0;
+int temp1A, temp1B, temp2A, temp2B, temp3A, temp3B, temp4A, temp4B = 0;
+
 MCP_CAN CAN(SPI_CAN);
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   while(CAN_OK != CAN.begin(CAN_1000KBPS))
   {
@@ -30,21 +33,16 @@ void loop() {
     //String ID = String(canID);
     //Serial.println(ID);
 
-    if(canID == 0x4C4) //Front sensor
+    if(canID == 0x4C4) //Front sensor (1220d)
     {
-      int temp1 = 0;
-      int temp2 = 0;
-      int temp3 = 0;
-      int temp4 = 0;
-
-      int temp1A = buf[0];
-      int temp1B = buf[1];
-      int temp2A = buf[2];
-      int temp2B = buf[3];
-      int temp3A = buf[4];
-      int temp3B = buf[5];
-      int temp4A = buf[6];
-      int temp4B = buf[7];
+      temp1A = buf[0];
+      temp1B = buf[1];
+      temp2A = buf[2];
+      temp2B = buf[3];
+      temp3A = buf[4];
+      temp3B = buf[5];
+      temp4A = buf[6];
+      temp4B = buf[7];
 
       temp1 = ((temp1A * 256) + temp1B)/10 - 100;
       temp2 = ((temp2A * 256) + temp2B)/10 - 100;
@@ -60,24 +58,19 @@ void loop() {
       Serial.println("0x4C4 (Front) Channel 2 Temp: " + alpha_temp2);
       Serial.println("0x4C4 (Front) Channel 3 Temp: " + alpha_temp3);
       Serial.println("0x4C4 (Front) Channel 4 Temp: " + alpha_temp4);
-      Serial.println();      
+      Serial.println();
     }
 
-    if(canID == 0x4C9) //Rear sensor
+    if(canID == 0x4C5) //Front sensor (1221d)
     {
-      int temp1 = 0;
-      int temp2 = 0;
-      int temp3 = 0;
-      int temp4 = 0;
-
-      int temp1A = buf[0];
-      int temp1B = buf[1];
-      int temp2A = buf[2];
-      int temp2B = buf[3];
-      int temp3A = buf[4];
-      int temp3B = buf[5];
-      int temp4A = buf[6];
-      int temp4B = buf[7];
+      temp1A = buf[0];
+      temp1B = buf[1];
+      temp2A = buf[2];
+      temp2B = buf[3];
+      temp3A = buf[4];
+      temp3B = buf[5];
+      temp4A = buf[6];
+      temp4B = buf[7];
 
       temp1 = ((temp1A * 256) + temp1B)/10 - 100;
       temp2 = ((temp2A * 256) + temp2B)/10 - 100;
@@ -89,23 +82,70 @@ void loop() {
       String alpha_temp3 = String(temp3);
       String alpha_temp4 = String(temp4);
 
-      Serial.println("0x4C9 (Rear) Channel 1 Temp: " + alpha_temp1);
-      Serial.println("0x4C9 (Rear) Channel 2 Temp: " + alpha_temp2);
-      Serial.println("0x4C9 (Rear) Channel 3 Temp: " + alpha_temp3);
-      Serial.println("0x4C9 (Rear) Channel 4 Temp: " + alpha_temp4);
-      Serial.println();      
+      Serial.println("0x4C5 (Front) Channel 5 Temp: " + alpha_temp1);
+      Serial.println("0x4C5 (Front) Channel 6 Temp: " + alpha_temp2);
+      Serial.println("0x4C5 (Front) Channel 7 Temp: " + alpha_temp3);
+      Serial.println("0x4C5 (Front) Channel 8 Temp: " + alpha_temp4);
+      Serial.println();
     }
 /*
-    if(canID == 0x4CD)
+    if(canID == 0x4CE) //Rear sensor (1230d)
     {
-      int temp = 0;
-      int tempA = buf[0];
-      int tempB = buf[1];
-      temp = ((tempA * 256) + tempB) / 10 - 100;  //data conversion to Celsius
-      String alpha_temp = String(temp);
-      Serial.println("0x4CD Sensor Temp: " + alpha_temp);
+      temp1A = buf[0];
+      temp1B = buf[1];
+      temp2A = buf[2];
+      temp2B = buf[3];
+      temp3A = buf[4];
+      temp3B = buf[5];
+      temp4A = buf[6];
+      temp4B = buf[7];
+
+      temp1 = ((temp1A * 256) + temp1B)/10 - 100;
+      temp2 = ((temp2A * 256) + temp2B)/10 - 100;
+      temp3 = ((temp3A * 256) + temp3B)/10 - 100;
+      temp4 = ((temp4A * 256) + temp4B)/10 - 100;
+
+      String alpha_temp1 = String(temp1);
+      String alpha_temp2 = String(temp2);
+      String alpha_temp3 = String(temp3);
+      String alpha_temp4 = String(temp4);
+
+      Serial.println("0x4CE (Rear) Channel 1 Temp: " + alpha_temp1);
+      Serial.println("0x4CE (Rear) Channel 2 Temp: " + alpha_temp2);
+      Serial.println("0x4CE (Rear) Channel 3 Temp: " + alpha_temp3);
+      Serial.println("0x4CE (Rear) Channel 4 Temp: " + alpha_temp4);
+      Serial.println();
     }
 
+    if(canID == 0x4CF) //Rear sensor (1231d)
+    {
+      temp1A = buf[0];
+      temp1B = buf[1];
+      temp2A = buf[2];
+      temp2B = buf[3];
+      temp3A = buf[4];
+      temp3B = buf[5];
+      temp4A = buf[6];
+      temp4B = buf[7];
+
+      temp1 = ((temp1A * 256) + temp1B)/10 - 100;
+      temp2 = ((temp2A * 256) + temp2B)/10 - 100;
+      temp3 = ((temp3A * 256) + temp3B)/10 - 100;
+      temp4 = ((temp4A * 256) + temp4B)/10 - 100;
+
+      String alpha_temp1 = String(temp1);
+      String alpha_temp2 = String(temp2);
+      String alpha_temp3 = String(temp3);
+      String alpha_temp4 = String(temp4);
+
+      Serial.println("0x4CF (Rear) Channel 5 Temp: " + alpha_temp1);
+      Serial.println("0x4CF (Rear) Channel 6 Temp: " + alpha_temp2);
+      Serial.println("0x4CF (Rear) Channel 7 Temp: " + alpha_temp3);
+      Serial.println("0x4CF (Rear) Channel 8 Temp: " + alpha_temp4);
+      Serial.println();
+    }
+*/
+/*
     if(canID == 0x4C8)
     {
       int temp = 0;
@@ -113,7 +153,17 @@ void loop() {
       int tempB = buf[1];
       temp = ((tempA * 256) + tempB) / 10 - 100;  //data conversion to Celsius
       String alpha_temp = String(temp);
-      Serial.println("0x4C8 Sensor Temp: " + alpha_temp);
+      Serial.println("Front Sensor Temp: " + alpha_temp);
+    }
+
+    if(canID == 0x4D2)
+    {
+      int temp = 0;
+      int tempA = buf[0];
+      int tempB = buf[1];
+      temp = ((tempA * 256) + tempB) / 10 - 100;  //data conversion to Celsius
+      String alpha_temp = String(temp);
+      Serial.println("Rear Sensor Temp: " + alpha_temp);
     }
 */
   }
--- Brake_Temp_Sensor_CAN_Config/Brake_Temp_Sensor_CAN_Config.ino
+++ Brake_Temp_Sensor_CAN_Config/Brake_Temp_Sensor_CAN_Config.ino
@@ -6,25 +6,25 @@
 
 MCP_CAN CAN(SPI_CAN);
 
-uint8_t pgmA = 0b01110101,      //MSB of 30000d or 0x7530h
-        pgmB = 0b00110000,      //LSB of 30000d or 0x7530h
-        frontIDA = 0b00000100,  //MSB of 1220d or 0x4C4h
-        frontIDB = 0b11000100,  //LSB of 1220d or 0x4C4h
-        rearIDA = 0b00000100,   //MSB of 1225d or 0x4C9h
-        rearIDB = 0b11001001,   //LSB of 1225d or 0x4C9h
-        emissivity = 90,        //Emissivity multiple of .01
-        sampling = 6,           //Sampling Frequency of 32Hz
-        channels = 40;          //4 channels
+uint8_t pgmA = 0b01110101,       //MSB of 30000d or 0x7530h
+        pgmB = 0b00110000,       //LSB of 30000d or 0x7530h
+        frontID_A = 0b00000100,  //MSB of 1220d or 0x4C4h
+        frontID_B = 0b11000100,  //LSB of 1220d or 0x4C4h
+        rearID_A = 0b00000100,   //MSB of 1225d or 0x4CEh
+        rearID_B = 0b11001110,   //LSB of 1225d or 0x4CEh
+        emissivity = 80,         //Emissivity multiple of .01
+        sampling = 4,            //Sampling Frequency of 8Hz
+        channels = 80;           //8 channels
 
 //CAN Data message to send
-uint8_t cfg_front[8] = {pgmA, pgmB, frontIDA, frontIDB, emissivity, sampling, channels, 0};
-uint8_t cfg_rear[8] = {pgmA, pgmB, rearIDA, rearIDB, emissivity, sampling, channels, 0};
+uint8_t cfg_front[8] = {pgmA, pgmB, frontID_A, frontID_B, emissivity, sampling, channels, 0};
+uint8_t cfg_rear[8] = {pgmA, pgmB, rearID_A, rearID_B, emissivity, sampling, channels, 0};
 
 int n = 1;
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   while(CAN_OK != CAN.begin(CAN_1000KBPS))
   {
@@ -38,17 +38,17 @@ void loop() {
   while(n == 1)
   {
     Serial.println("Configuring Front...");
-    for(int i = 0; i <= 12; i++)
+    for(int i = 0; i <= 12; i++)  //Send a total of 12 times just in case
     {
       CAN.sendMsgBuf(0x4C4, 0, 8, cfg_front); //(Target address, Standard frame, Length of frame, Data array)
-      delay(1000);
+      delay(1000);  //Send 1Hz interval
     }
     Serial.println("Front Config Done!");
 
     Serial.println("Configuring Rear...");
     for(int i = 0; i <= 12; i++)
     {
-      CAN.sendMsgBuf(0x4C9, 0, 8, cfg_rear);
+      CAN.sendMsgBuf(0x4CE, 0, 8, cfg_rear);
       delay(1000);
     }
     Serial.println("Rear Config Done!");
